5db86433a012fbfcd184b8bd9435df1c9f30075c
07-ankur <im4ankur@gmail.com>
2023-10-08 23:27:10 +0530

    Connected DB

--- server.js
+++ server.js
@@ -1,7 +1,27 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+
 const app = require('./app');
 
+// console.log(process.env)
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+
 dotenv.config({ path: './config.env' });
+
+mongoose
+  // .connect(process.env.DATABASE_LOCAL, {    // LOCAL DATABASE
+  .connect(DB, {
+    // HOSTED DATABASE
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log('DB connection successful!');
+  });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
